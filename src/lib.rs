f58f868292699ce44a302dea0cc1fdd94f830e93
thara <git@thara.dev>
2021-07-04 21:15:04 +0900

    Update reqwest version

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,8 @@ extern crate reqwest;
 const GITIGNORE_BASEURL: &str = "https://www.gitignore.io/api";
 const GITIGNORE_LIST_URL: &str = "https://www.gitignore.io/api/list";
 
-pub fn list() -> reqwest::Result<reqwest::Response> {
-    reqwest::get(GITIGNORE_LIST_URL)
-}
-
-pub fn list_by(client: reqwest::Client) -> reqwest::Result<reqwest::Response> {
-    client.get(GITIGNORE_LIST_URL).send()
+pub fn list() -> reqwest::Result<reqwest::blocking::Response> {
+    reqwest::blocking::get(GITIGNORE_LIST_URL)
 }
 
 pub fn gen_url(search_items: &Vec<String>) -> String {
@@ -18,14 +14,7 @@ pub fn gen_url(search_items: &Vec<String>) -> String {
     base_url + "/" + &search_strs
 }
 
-pub fn search(search_items: &Vec<String>) -> reqwest::Result<reqwest::Response> {
+pub fn search(search_items: &Vec<String>) -> reqwest::Result<reqwest::blocking::Response> {
     let url = gen_url(search_items);
-    reqwest::get(&url)
-}
-
-pub fn search_by(client: reqwest::Client,
-                 search_items: &Vec<String>)
-                 -> reqwest::Result<reqwest::Response> {
-    let url = gen_url(search_items);
-    client.get(&url).send()
+    reqwest::blocking::get(&url)
 }
