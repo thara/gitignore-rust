d9df82d2fc263769bdc36aebcaae37b152b4874b
thara <git@thara.dev>
2021-07-04 21:20:15 +0900

    Remove unnecessary depedencies

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
 use std::env;
 
-#[macro_use]
-extern crate error_chain;
-extern crate env_logger;
-
 extern crate ignor;
 extern crate reqwest;
 
-error_chain!{
-    foreign_links {
-        ReqError(reqwest::Error);
-        IoError(std::io::Error);
-    }
-}
-
-fn run() -> Result<()> {
-    env_logger::init().expect("Failed to initialize logger");
-
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().skip(1).collect();
 
     let res = if args.len() == 0 {
@@ -28,5 +15,3 @@ fn run() -> Result<()> {
     let _ = std::io::copy(&mut res?, &mut std::io::stdout())?;
     Ok(())
 }
-
-quick_main!(run);
